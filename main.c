bd36255b0fc4e92176dd96959ecbf77179f09869
Jon <jonchoukroun@gmail.com>
2019-12-27 12:38:43 -0500

    Build out emulation cycle

--- main.c
+++ main.c
@@ -2,24 +2,54 @@
 #include "chip8_io.h"
 #include "cpu.h"
 
+uint8 runCycle(struct CPU *);
+
 int main(int argc, char const *argv[]) {
 
-  // struct CPU cpu = initialize();
-  // initializeDisplay();
+  struct CPU *cpu = initialize();
+  WINDOW *window = initializeDisplay();
+  struct HashTable *keyTable = initializeInput();
 
-  // for (;;) {
-  //   uint8 keyPress = getKeyPress();
+  uint8 running = 1;
+  while (running) {
+    setKeyState(window, cpu->keyState, keyTable);
 
-  //   if (keyPress == 0x71) { break; }
-  // }
+    running = runCycle(cpu);
+    if (running == 0) {
+      // debugger: require keypress to end program
+      printw("No opcode match\n");
+      getch();
+      break;
+    }
 
-  // uint16 opcode = fetchOpcode(&cpu);
-  // printf("op %x\n", opcode);
-  // readOpcode(opcode, &cpu);
+    if (cpu->drawFlag == 1) {
+      drawFrameBuffer(window, cpu->frameBuffer);
+      cpu->drawFlag = 0;
+    }
+  }
 
-  struct HashTable *keyTable = createKeyTable();
+  destroyIO(window, keyTable);
 
-  // closeDisplay();
+  // printf("%x\n", cpu->programCounter);
+  // uint16 opcode = fetchOpcode(cpu);
+  // readOpcode(opcode, cpu);
 
+  destroyCPU(cpu);
   return 0;
+}
+
+uint8 runCycle(struct CPU *cpu) {
+  uint16 opcode = fetchOpcode(cpu);
+  uint8 status = executeOpcode(cpu, opcode);
+  if (status == 0) { return 0; }
+
+  if (cpu->delayTimer > 0) {
+    --cpu->delayTimer;
+  }
+
+  if (cpu->soundTimer > 0) {
+    --cpu->soundTimer;
+  }
+
+  return 1;
 }
