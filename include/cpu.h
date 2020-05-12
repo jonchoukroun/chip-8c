b9ebc2c30f5ff8d9263a71462515bc7ac74af1fe
Jon <jonchoukroun@gmail.com>
2020-05-12 00:06:17 -0700

    Remove cycle management

--- include/cpu.h
+++ include/cpu.h
@@ -2,8 +2,6 @@
 #define CHIP_8
 
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 #include "input.h"
 #include "output.h"
 
@@ -36,29 +34,6 @@ uint16 fetch_opcode(CPU *);
 uint8 execute_opcode(CPU *, uint16);
 
 void destroy_cpu(CPU *);
-
-// Cycle management
-enum CycleType {
-    CLOCK_CYCLE,
-    TIMER_CYCLE
-};
-
-typedef struct Cycle {
-    uint8 type;
-    uint16 chunk;
-    clock_t start;
-    double elapsed;
-} Cycle;
-
-Cycle * create_cycle(uint8);
-
-void update_cycle(Cycle *);
-
-void delay(Cycle *);
-
-void reset_cycle(Cycle *);
-
-void destroy_cycle(Cycle *);
 
 #endif
 
